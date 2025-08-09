4fe8f9aafecb878eb21435073f125ee8714dee03
NirmalMishra08 <145590866+NirmalMishra08@users.noreply.github.com>
2025-08-09 22:36:47 +0530

    working on it

---
+++ apps/web/modules/auth/ui/components/authguard.tsx
@@ -0,0 +1,24 @@
+"use client"
+import { Authenticated, AuthLoading, Unauthenticated } from "convex/react"
+import { AuthLayout } from "../layout/auth.layout"
+import { SignInView } from "../views/sign-in-view"
+
+export const AuthGuard = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <>
+            <AuthLoading>
+                <AuthLayout>
+                    <p>Loading ...</p>
+                </AuthLayout>
+            </AuthLoading>
+            <Authenticated>
+                {children}
+            </Authenticated>
+            <AuthLoading>
+                <AuthLayout>
+                    <SignInView />
+                </AuthLayout>
+            </AuthLoading>
+        </>
+    )
+}
